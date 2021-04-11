899b12116a28d208f4000e82e1439fec63c2e0f0
=Hasan <hasanozkilic23@outlook.com>
2021-04-11 15:03:17 +0300

    Commit 2

---
+++ backendSide/Business/Concrete/CategoryManager.cs
@@ -0,0 +1,16 @@
+﻿using Business.Abstract;
+using Business.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        public IResult Add()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
